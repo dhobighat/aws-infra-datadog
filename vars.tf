9ca3bc42cbc07bfce005b96fdffcd5a8fa7e2d0d
sandy007sandipan <$Passw0rd$>
2020-12-29 17:42:50 +0530

    Initial Commit

---
+++ vars.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+  description = "Default AWS Region to create the resources"
+  default     = "us-east-1"
+}
+
+variable "env" {
+  default = "dev"
+}
+variable "identifier" {
+  default = "datadog-monitor"
+}
+variable "datadog-api-key" {
+  default = "67e1a9fd1da742a44984cd742b9dbe5a"
+}
+variable "datadog-extra-config" {
+  default = "./init"
+}
+variable "ecs-cluster-id" {
+  default = "arn:aws:ecs:us-east-1:666519825349:cluster/aws-ecs-dev"
+}
